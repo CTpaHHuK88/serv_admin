539ab6334d34318e73ed04a4bc930c7086e8f483
Admin <8338880@gmail.com>
2025-04-02 16:55:14 +0300

    bot_update

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-import os, sys, psutil
+import os, sys
 import datetime
 import telebot
 from bot_config import DATA
+import psutil
 from psutil._common import bytes2human
 
 dat = DATA()
@@ -12,8 +13,13 @@ def main():
     temps = psutil.sensors_temperatures()
     for name, entries in temps.items():
         for entry in entries:
-            if entry.current > 20:
+            if entry.current > 40:
                 bot.send_message(dat.CHAT_ID, f"Температура превышена!\nТекущая температура CPU {entry.current} C")
+                status = 1
+            if entry.current < 39:
+                status = 0
+
 
 if __name__ == '__main__':
     main()
+    bot.polling()
